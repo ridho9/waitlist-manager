f5b7d1f4212f6d85e11f3ca6a548e48b1c8026a5
ridho9 <p.ridho9@gmail.com>
2024-11-28 13:29:18 +0700

    setup framework

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,6 +1,8 @@
+import os
 from typing import Union
 
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -13,3 +15,7 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=int(os.getenv("BE_PORT", 8002)))
